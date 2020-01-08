93351f02a38726e42fa3f3cc23c032ad2ddee207
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-01-08 14:12:09 -0800

    Added nested-admin

--- pseudosci/admin.py
+++ pseudosci/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+import nested_admin
+
+from pseudosci.models import Quiz, Question, Choice
 
 # Register your models here.
+
+class ChoiceInline(nested_admin.NestedTabularInline):
+    model = Choice
+    extra = 4
+
+class QuestionInline(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [ChoiceInline]
+    extra = 0
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    """Show helpful fields on the changelist page."""
+    list_display = ('title', 'author', 'created', 'modified')
+    inlines = [QuestionInline]
+    search_fields = ['prompt']
+
+admin.site.register(Quiz, QuizAdmin)
--- pseudosci/models.py
+++ pseudosci/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
+class Quiz(models.Model):
+    title = models.CharField('Title', max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    slug = models.CharField(max_length=120, blank=True, editable=False)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = ("Quiz")
+        verbose_name_plural = ("Quizzes")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """ Returns a fully-qualified path for a page (/what-are-you). """
+        path_components = {'slug': self.slug}
+        return reverse('quiz-page', kwargs={path_components})
+    
+    def save(self, *args, **kwargs):
+        """ Creates a URL safe slug automatically when a new page is created. """
+        if not self.pk:
+            self.slug = slugify(self.title, allow_unicode=True)
+
+        # Call save on the superclass.
+        return super(Quiz, self).save(*args, **kwargs)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    prompt = models.CharField('Question', max_length=255)
+
+    def __str__(self):
+        return self.prompt
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
