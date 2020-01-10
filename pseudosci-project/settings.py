7f05ec98bb69f28e2de134f1cd7bde4be16416ab
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-01-09 20:50:41 -0800

    Make it better

--- pseudosci-project/settings.py
+++ pseudosci-project/settings.py
@@ -23,7 +23,7 @@ SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'SO_MANY_SECRETS')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
