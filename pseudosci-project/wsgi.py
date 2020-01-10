357f0225ea1f50f9cef96fa5d0862ca51519ab88
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-01-09 21:04:36 -0800

    Fixed secret key stuff

--- pseudosci-project/wsgi.py
+++ pseudosci-project/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
-
-load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pseudosci-project.settings')
 
